1264e1bdea21137dae7df3f89b915cb19d568a2c
Dimanok1989 <kolgaev@gmail.com>
2021-03-11 16:00:11 +0300

    Вывод иконки бота в строках посещения сайта

--- src/components/Admin/AdCenter/VisitListRow.jsx
+++ src/components/Admin/AdCenter/VisitListRow.jsx
@@ -12,9 +12,12 @@ export default function VisitListRow(props) {
 
     return <List.Item className="d-flex justify-content-between align-items-center px-2">
 
-        <List.Icon name={icon} size="large" verticalAlign="middle" />
+        <div className="d-flex justify-content-center flex-column text-center" style={{ marginRight: ".5rem" }}>
+            <div><List.Icon name={icon} size="large" style={{ margin: 0 }} /></div>
+            {row.bot ? <div className="mt-1"><List.Icon name="android" color="green" style={{ margin: 0 }} title="Бот" /></div> : null}
+        </div>
 
-        <List.Content>
+        <List.Content className="w-100">
             <List.Header><a href={`//` + row.site + row.page} target="_blank">{row.site}{row.page}</a></List.Header>
             {row.utm_term ? <List.Description><strong>Utm term</strong>{' '}{row.utm_term}</List.Description> : null}
             {row.user_agent ? <List.Description><small className="text-wrap">{row.user_agent}</small></List.Description> : null}
