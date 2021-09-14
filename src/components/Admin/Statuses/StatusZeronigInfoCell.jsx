06a4ae7b2e805175e02bbab2024de53f26d3d5e1
Dimanok1989 <kolgaev@gmail.com>
2021-09-14 11:11:17 +0300

    Вывод информации об обнулении

--- src/components/Admin/Statuses/StatusZeronigInfoCell.jsx
+++ src/components/Admin/Statuses/StatusZeronigInfoCell.jsx
@@ -1,12 +1,36 @@
+import { Icon } from "semantic-ui-react";
+
 function StatusZeronigInfoCell(props) {
 
     const { zeroing, data } = props;
 
     if (zeroing === 0)
-        return null;
+        return <Icon name="times circle outline" style={{ opacity: ".5" }} title="Обнуление не настроено" />;
 
-    return <div>
-        Обнуление
+    if (data.algorithm === "xHour") {
+        return <div className="d-flex justify-content-center align-items-center">
+            <span><Icon name="clock outline" color="green" /></span>
+            <span>Через <code>n</code> часов: <b>{data.algorithm_option}</b></span>
+        </div>
+    }
+
+    if (data.algorithm === "nextDay") {
+        return <div className="d-flex justify-content-center align-items-center">
+            <span><Icon name="calendar check outline" color="green" /></span>
+            <span>На следующий день</span>
+        </div>
+    }
+
+    if (data.algorithm === "xDays") {
+        return <div className="d-flex justify-content-center align-items-center">
+            <span><Icon name="calendar alternate outline" color="green" /></span>
+            <span>Через <code>n</code> дней: <b>{data.algorithm_option}</b></span>
+        </div>
+    }
+
+    return <div className="d-flex justify-content-center align-items-center">
+        <span><Icon name="check" color="green" /></span>
+        <span>{data.algorithm}</span>
     </div>
 
 }
