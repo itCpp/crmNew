d64c034c3e6c3adaee492fb20d4ab551d247e85c
Dimanok1989 <kolgaev@gmail.com>
2021-12-02 10:51:51 +0300

    Вывод адресов

--- src/components/Admin/Block/BlockDrive.jsx
+++ src/components/Admin/Block/BlockDrive.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import axios from "./../../../utils/axios-header";
-import { Header, Loader, Message, Input } from "semantic-ui-react";
+import { Header, Loader, Message, Input, Checkbox, Icon } from "semantic-ui-react";
 
 export default (props => {
 
@@ -27,7 +27,7 @@ export default (props => {
         getRows({});
     }, []);
 
-    return <div className="segment-compact">
+    return <div style={{ maxWidth: 600 }}>
 
         <div className="admin-content-segment d-flex justify-content-between align-items-center">
 
@@ -56,6 +56,24 @@ export default (props => {
         {!loading && !error && rows.length === 0 && <Message content="Ничего не найдено" />}
 
         {!loading && !error && rows.length > 0 && <div>
+            {rows.map(row => <div key={row.id} className="d-flex justify-content-between align-items-center admin-content-segment">
+
+                <h4 className="m-0">
+                    {row.block === 1 && <Icon name="ban" color="red" title="Заблокировано" />}
+                    {row.is_hostname === 0
+                        ? <a style={{ cursor: "pointer" }} onClick={() => props.history.push(`/admin/block/ip?addr=${row.host}`)}>{row.host}</a>
+                        : row.host
+                    }
+
+                </h4>
+
+                <Checkbox
+                    toggle
+                    checked={row.block === 1}
+                    onChange={console.log}
+                />
+
+            </div>)}
         </div>}
 
     </div>
