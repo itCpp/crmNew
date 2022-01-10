a32c568eaa694de9c376e9d56108fc15dc17d713
Dimanok1989 <kolgaev@gmail.com>
2022-01-10 11:24:10 +0300

    вывод новых заявок на личной странице

--- src/components/CRM/User/Alerts/AlertsRequests.jsx
+++ src/components/CRM/User/Alerts/AlertsRequests.jsx
@@ -1,15 +1,38 @@
 import { Segment } from "../../UI";
-import { Table } from "semantic-ui-react";
+import { Icon } from "semantic-ui-react";
+import moment from "moment";
+import { withRouter } from "react-router-dom";
+import QueryTypeIcon from "./../../Requests/RequestsDataTableCells/Elements/QueryTypeIcon";
 
-const RequestRow = props => {
+const RequestRow = withRouter(props => {
 
     const { row } = props;
 
-    return <div className="segment-list-row">
-        <div>#{row.id}</div>
+    return <div className={`segment-list-row request-row-theme-${row.status && row.status.theme || 0} px-3 py-2 rounded my-1`} onClick={() => props.history.push(`/requests?id=${row.id}`)}>
+        <div className="d-flex justify-content-between align-items-center">
+            <span>
+                <QueryTypeIcon query_type={row.query_type} />
+                <strong>#{row.id}</strong>
+                {row.source && <span className="ml-2" title={row.source.name}>{row.source.name}</span>}
+            </span>
+            <small className="opacity-80" title="Дата назначения">
+                <Icon name="plus" />
+                {moment(row.set_at).format("DD.MM.YYYY HH:mm")}
+            </small>
+        </div>
+        <div className="d-flex align-items-center flex-wrap">
+            {row.client_name && <span className="mr-3 d-flex">
+                <Icon name="user" className="opacity-50" />
+                {row.client_name}
+            </span>}
+            {row.comment && <span title="Комментарий" className="d-flex">
+                <Icon name="quote right" className="opacity-50" />
+                {row.comment}
+            </span>}
+        </div>
     </div>
 
-}
+});
 
 export default (({ requests, height }) => {
 
@@ -24,7 +47,7 @@ export default (({ requests, height }) => {
     >
 
         <div className="segmet-list">
-            {requests.map((row, i) => <RequestRow key={`${row.id}_new_${i}`} row={row} />)}
+            {requests && requests.map((row, i) => <RequestRow key={`${row.id}_new_${i}`} row={row} />)}
         </div>
 
     </Segment>;
