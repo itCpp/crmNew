dc305242fa26a307794c1a6a7162fa9a498cea01
Dimanok1989 <kolgaev@gmail.com>
2022-02-07 17:37:06 +0300

    Обновлние данных меню админки

--- src/components/Admin/AdminMenu.jsx
+++ src/components/Admin/AdminMenu.jsx
@@ -22,58 +22,55 @@ function AdminMenu(props) {
 
     return <div className="admin-menu">
 
-        {permits.block_dev
-            ? <div className="admin-menu-block">
+        {(permits.block_dev || permits.admin_users) && <div className="admin-menu-block">
 
-                <h5>Учетные записи</h5>
+            <h5>Учетные записи</h5>
 
+            {(permits.block_dev || permits.admin_users) &&
                 <NavLink to="/admin/users" className="admin-menu-point" onClick={changePage}>
                     <Icon name="user" />
                     <span>Сотрудники</span>
                 </NavLink>
+            }
 
-                {/* <NavLink to="/admin/permits" className="admin-menu-point" onClick={changePage}>
+            {/* <NavLink to="/admin/permits" className="admin-menu-point" onClick={changePage}>
+                <Icon name="angle right" />
+                <span>Сессии</span>
+            </NavLink> */}
+
+            {permits.dev_roles &&
+                <NavLink to="/admin/roles" className="admin-menu-point" onClick={changePage}>
                     <Icon name="angle right" />
-                    <span>Сессии</span>
-                </NavLink> */}
-
-                {permits.block_dev &&
-                    <NavLink to="/admin/roles" className="admin-menu-point" onClick={changePage}>
-                        <Icon name="angle right" />
-                        <span>Roles</span>
-                    </NavLink>
-                }
-
-                {permits.block_dev &&
-                    <NavLink to="/admin/permits" className="admin-menu-point" onClick={changePage}>
-                        <Icon name="angle right" />
-                        <span>Permissions</span>
-                    </NavLink>
-                }
-
-            </div>
-            : null
-        }
-
-        {permits.block_dev &&
-            <div className="admin-menu-block">
-
-                <h5>Логи</h5>
-
-                {permits.dev_calls &&
-                    <NavLink to="/admin/calls" className="admin-menu-point" onClick={changePage}>
-                        <Icon name="call" />
-                        <span>Звонки</span>
-                    </NavLink>
-                }
-
-                <NavLink to="/admin/sips" className="admin-menu-point" onClick={changePage}>
-                    <Icon name="phone volume" />
-                    <span>Активность SIP</span>
+                    <span>Roles</span>
                 </NavLink>
+            }
 
-            </div>
-        }
+            {permits.dev_permits &&
+                <NavLink to="/admin/permits" className="admin-menu-point" onClick={changePage}>
+                    <Icon name="angle right" />
+                    <span>Permissions</span>
+                </NavLink>
+            }
+
+        </div>}
+
+        {permits.block_dev && <div className="admin-menu-block">
+
+            <h5>Логи</h5>
+
+            {permits.dev_calls &&
+                <NavLink to="/admin/calls" className="admin-menu-point" onClick={changePage}>
+                    <Icon name="call" />
+                    <span>Звонки</span>
+                </NavLink>
+            }
+
+            <NavLink to="/admin/sips" className="admin-menu-point" onClick={changePage}>
+                <Icon name="phone volume" />
+                <span>Активность SIP</span>
+            </NavLink>
+
+        </div>}
 
         {permits.block_dev && <div className="admin-menu-block">
 
@@ -101,54 +98,51 @@ function AdminMenu(props) {
 
         </div>}
 
-        {permits.block_dev
-            ? <div className="admin-menu-block">
+        {permits.block_dev && <div className="admin-menu-block">
 
-                <h5>Настройки</h5>
+            <h5>Настройки</h5>
 
-                <NavLink to="/admin/settings" className="admin-menu-point" onClick={changePage}>
-                    <Icon name="settings" />
-                    <span>Глобальные настройки</span>
-                </NavLink>
+            <NavLink to="/admin/settings" className="admin-menu-point" onClick={changePage}>
+                <Icon name="settings" />
+                <span>Глобальные настройки</span>
+            </NavLink>
 
-                <NavLink to="/admin/callcenters" className="admin-menu-point" onClick={changePage}>
-                    <Icon name="sound" />
-                    <span>Колл-центры</span>
-                </NavLink>              
+            <NavLink to="/admin/callcenters" className="admin-menu-point" onClick={changePage}>
+                <Icon name="sound" />
+                <span>Колл-центры</span>
+            </NavLink>
 
-                <NavLink to="/admin/sources" className="admin-menu-point" onClick={changePage}>
-                    <Icon name="fork" />
-                    <span>Источники</span>
-                </NavLink>
+            <NavLink to="/admin/sources" className="admin-menu-point" onClick={changePage}>
+                <Icon name="fork" />
+                <span>Источники</span>
+            </NavLink>
 
-                <NavLink to="/admin/databases" className="admin-menu-point" onClick={changePage}>
-                    <Icon name="database" />
-                    <span>Базы сайтов</span>
-                </NavLink>
+            <NavLink to="/admin/databases" className="admin-menu-point" onClick={changePage}>
+                <Icon name="database" />
+                <span>Базы сайтов</span>
+            </NavLink>
 
-                <NavLink to="/admin/statuses" className="admin-menu-point" onClick={changePage}>
-                    <Icon name="certificate" />
-                    <span>Статусы</span>
-                </NavLink>
+            <NavLink to="/admin/statuses" className="admin-menu-point" onClick={changePage}>
+                <Icon name="certificate" />
+                <span>Статусы</span>
+            </NavLink>
 
-                <NavLink to="/admin/tabs" className="admin-menu-point" onClick={changePage}>
-                    <Icon name="table" />
-                    <span>Вкладки</span>
-                </NavLink>
+            <NavLink to="/admin/tabs" className="admin-menu-point" onClick={changePage}>
+                <Icon name="table" />
+                <span>Вкладки</span>
+            </NavLink>
 
-                <NavLink to="/admin/office" className="admin-menu-point" onClick={changePage}>
-                    <Icon name="building" />
-                    <span>Офисы</span>
-                </NavLink>
+            <NavLink to="/admin/office" className="admin-menu-point" onClick={changePage}>
+                <Icon name="building" />
+                <span>Офисы</span>
+            </NavLink>
 
-                <NavLink to="/admin/callsqueue" className="admin-menu-point" onClick={changePage}>
-                    <Icon name="random" />
-                    <span>Направление звоков</span>
-                </NavLink>
+            <NavLink to="/admin/callsqueue" className="admin-menu-point" onClick={changePage}>
+                <Icon name="random" />
+                <span>Направление звоков</span>
+            </NavLink>
 
-            </div>
-            : null
-        }
+        </div>}
 
         {permits.block_dev && <div className="admin-menu-block">
 
