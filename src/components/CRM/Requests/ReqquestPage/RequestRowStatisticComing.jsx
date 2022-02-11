8638a713a7bf387e7ec16aa2ea9404879964dfdc
Dimanok1989 <kolgaev@gmail.com>
2022-02-11 13:58:23 +0300

    Вывод других приходов клиента

--- src/components/CRM/Requests/ReqquestPage/RequestRowStatisticComing.jsx
+++ src/components/CRM/Requests/ReqquestPage/RequestRowStatisticComing.jsx
@@ -1,31 +1,76 @@
-import { Grid, Header } from "semantic-ui-react";
+import { Grid, Header, Image } from "semantic-ui-react";
 import moment from "moment";
 
 const RequestRowStatisticComing = props => {
 
-    const { data } = props;
     const coming = props.data?.statistic?.coming || null;
 
-    return <Grid.Column>
+    const comings = coming?.comings || [];
+    const last = comings[comings.length - 1] || null;
 
-        <div className="block-card py-3">
+    return <>
 
-            <Header
-                as="h4"
-                content="Приход"
-                subheader={coming?.date && moment(coming.date).format("DD MMMM YYYY")}
-                className="mb-1"
-                color="green"
-            />
+        <Grid.Column>
 
-            {!coming && <div className="opacity-50 my-3">Приход не найден</div>}
+            <div className="block-card py-3">
 
-            {coming?.start && <div>Время прихода: <b>{coming.start}</b></div>}
-            {coming?.start && <div>Время ухода: {coming?.stop ? <b>{coming.stop}</b> : <i className="text-warning">нет</i>}</div>}
+                <Header
+                    as="h3"
+                    content="Приход"
+                    className="mb-1"
+                    color="green"
+                />
 
-        </div>
+                {!coming && <div className="opacity-50 my-3">Приход не найден</div>}
 
-    </Grid.Column>
+                {coming?.date && <div>{moment(coming.date).format("DD MMMM YYYY")}</div>}
+                {coming?.start && <div>Время прихода: <b>{coming.start}</b></div>}
+                {coming?.start && <div>Время ухода: {coming?.stop ? <b>{coming.stop}</b> : <i className="text-warning">нет</i>}</div>}
+
+            </div>
+
+        </Grid.Column>
+
+        <Grid.Column>
+
+            <div className="block-card py-3">
+
+                <Header
+                    as="h3"
+                    content="Приходы клиента"
+                    className="mb-1"
+                    color="green"
+                />
+
+                {comings.length === 0 && <div className="opacity-50 my-3">Других приходов нет</div>}
+
+                {comings.length > 0 && <div>
+
+                    <div>Всего приходов <b>{comings.length}</b></div>
+
+                    {last !== null && <>
+                        <div className="mt-1">Последний приход:</div>
+                        <div className="d-flex align-items-center">
+                            <Image
+                                src={last?.company?.icon}
+                                rounded
+                                width={16}
+                                height={16}
+                                title={last?.company?.name || "Неизвестно"}
+                                className="mr-2"
+                            />
+                            <span className="mr-2">{moment(last.date).format("DD.MM.YYYY")}</span>
+                            <strong>{last.pin}</strong>
+                        </div>
+                    </>}
+
+                </div>}
+
+            </div>
+
+        </Grid.Column>
+
+    </>
 
 }
 
