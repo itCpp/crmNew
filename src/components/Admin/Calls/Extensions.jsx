6851b856a318d629da79a605bec6e4f87d70e916
Dimanok1989 <kolgaev@gmail.com>
2022-04-18 14:35:18 +0300

    Отображение рекламных площадок для слушателей звонков

--- src/components/Admin/Calls/Extensions.jsx
+++ src/components/Admin/Calls/Extensions.jsx
@@ -59,25 +59,38 @@ const Extensions = props => {
             <List divided relaxed>
                 {extensions.map(row => <List.Item key={row.id}>
                     <div className="d-flex justify-content-between align-items-center list-extensions px-2">
+
                         {row.on_work !== 1 && <div title="Выключен"><Icon name="power off" /></div>}
+
                         <List.Content className="flex-grow-1" style={{ opacity: row.on_work !== 1 ? 0.5 : 1 }}>
                             <List.Header as="strong">{row.extension}</List.Header>
                             <List.Description as="i">{row.comment}</List.Description>
                         </List.Content>
+
+                        {row.ad_place && <div style={{ width: 20 }} className="text-center">
+                            <Icon
+                                name={row.ad_place}
+                                disabled
+                                fitted
+                            />
+                        </div>}
+
                         <div title="Номер телефона источника"><Icon name="fork" />{row.phone}</div>
+
                         <div style={{ textAlign: "center", minWidth: 100 }}>
-                            <Label title="Количество добавленных заявок">{row.added}</Label>
+                            <Label title="Количество добавленных заявок">{row.added || 0}</Label>
                         </div>
+
                         <div>
-                            <Button
-                                basic
-                                size="tiny"
-                                circular
-                                icon="pencil"
-                                title="Изменить"
+                            <Icon
+                                name="pencil"
+                                title="Редактировать"
+                                fitted
+                                link
                                 onClick={() => setExtension(row.id)}
                             />
                         </div>
+
                     </div>
                 </List.Item>)}
             </List>
